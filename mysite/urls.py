cd5f5729a904eded9e3e25d8a090f837af71e218
Hemanth Chowdary <hemanthchowdary132@gmail.com>
2026-04-01 00:52:36 +0530

    redirect root to /blog/

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/blog/')),
     path('admin/', admin.site.urls),
     path("blog/", include("blog.urls", namespace="blog")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
